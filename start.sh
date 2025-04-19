f9b9763001d7c965e437dda8a0a8a62d8485379e
Anthony Barbier <anthonyb@graphcore.ai>
2025-04-19 17:39:56 +0100

    switch to TF for chromebook

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@ DIR=$HOME/bark-monitor/
 cd $DIR
 . venv/bin/activate
 cd bark_monitor
-bark-monitor-amplitude --config ../bark-monitor.json 2>&1 | ts
+bark-monitor --config ../bark-monitor.json 2>&1 | ts
 
--- start_chromebook.sh
+++ start_chromebook.sh
@@ -2,4 +2,4 @@ DIR=$HOME/bark-monitor/
 cd $DIR
 . venv/bin/activate
 cd bark_monitor
-bark-monitor-amplitude --config ../bark_chromebook.json 2>&1 | ts
+bark-monitor --config ../bark_chromebook.json 2>&1 | ts
